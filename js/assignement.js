87a3aa0c616f0286c50a5262dc352272c02cbda9
prasanthyethirajula <kamugarisk@gmail.com>
2024-02-27 20:30:45 -0500

    assignement

---
+++ js/assignement.js
@@ -0,0 +1,65 @@
+// factorial
+
+// let number = 4;
+
+// let factorial = 1;
+
+// if (number < 0) {
+//     console.log("can't find factorial for negative numbers");
+
+// } else {
+
+//     for (let num = 2; num <= number; num++) {
+//         factorial = factorial * num;
+//     }
+
+//     console.log('${ number }! = ${ factorial }');
+// }
+
+
+// // palindrome
+
+// let word = "racecar";
+// let letters = word.split("");
+
+// let reversedletters = letters.reverse();
+
+// let reversedWord = reversedletters.join("");
+
+// if (word() == reversedWord()) {
+//     console.log("It's a palindrome");
+// } else {
+//     console.log("It's not a palindrome");
+// }
+
+
+// finding maximum number
+
+// function maximum() {
+//     let array = [12, 19, 26, 16, 45, 97];
+//     let max = array[0];
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] > max) {
+//             max = array[i];
+//         }
+
+//     }
+
+//     console.log("the maximum number in array is ${max}");
+// }
+
+// maximum();
+
+
+// reverse array
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// numbers.reverse();
+
+// console.log(numbers);
+
+
+//  fibonacci series
+
+
